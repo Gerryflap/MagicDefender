a1dc0ab7c8119e5ff58c9c089b15d8218aee6477
gerben <gerry2211@gmail.com>
2024-01-04 11:36:13 +0100

    Made camera work with zoom and movement, repeated background texture

---
+++ scenes/user_camera.gd
@@ -0,0 +1,37 @@
+extends Camera2D
+
+# Camera speed in px/s
+@export var camera_speed = 400
+
+# Zoom base. Zoom level is vased on zoom_base ** zoom_exponent
+@export var zoom_base = 1.2
+
+var zoom_exponent = 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var movement = Vector2(0,0)
+	
+	if Input.is_action_pressed("camera_down"):
+		movement.y += 1
+	if Input.is_action_pressed("camera_up"):
+		movement.y -= 1
+	if Input.is_action_pressed("camera_left"):
+		movement.x -= 1	
+	if Input.is_action_pressed("camera_right"):
+		movement.x += 1
+	
+	movement = movement.normalized() * delta * camera_speed * (zoom_base ** zoom_exponent)
+	position += movement
+
+func _input(event):
+	if event.is_action_pressed("camera_zoom_in"):
+		zoom_exponent -= 1
+	if event.is_action_pressed("camera_zoom_out"):
+		zoom_exponent += 1
+	zoom = Vector2(1,1) * zoom_base ** -zoom_exponent
