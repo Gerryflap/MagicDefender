470f98788dd46a00e5b682619e250469ee8125fa
gerben <gerry2211@gmail.com>
2024-01-05 13:15:00 +0100

    Updated spritework and zombie enemy

--- scenes/Enemies.gd
+++ scenes/Enemies.gd
@@ -4,9 +4,9 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	for i in range(1000):
+	for i in range(500):
 		var enemy = enemy_scene.instantiate()
-		enemy.position = Vector2(randf_range(0, 100), randf_range(0, 1500))
+		enemy.position = Vector2(randf_range(-1000, -100), randf_range(600, 1300))
 		add_child(enemy)
 
 
--- scenes/Enemy.gd
+++ scenes/Enemy.gd
@@ -2,10 +2,13 @@ extends RigidBody2D
 
 @export var change_dir_min_s: float = 10
 @export var change_dir_max_s: float = 1
-var max_speed: float = 1000
-var speed: float = 0
+var max_speed: float = 40
+var desired_speed: float = 0
 var change_dir_counter: float = 0
 var direction: float = 0.0
+var previous_pos = Vector2(0,0)
+var previous_delta: float = 0
+var impatience = 0.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -13,20 +16,42 @@ func _ready():
 	reset_change_dir()
 	$AnimatedSprite2D.frame = randf_range(0, 2)
 	$AnimatedSprite2D.play()
-	speed = randf_range(max_speed/2, max_speed)
+	desired_speed = randf_range(max_speed/2, max_speed)
+	previous_pos = position
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	linear_velocity = Vector2(cos(direction), sin(direction)) * speed * delta
+	linear_velocity = Vector2(cos(direction), sin(direction)) * desired_speed
 	
+	impatience -= delta
+	if previous_delta != 0:
+		var prev_speed = (position - previous_pos).length() / previous_delta
+		if prev_speed < (desired_speed / 2.0):
+			impatience += delta * 2.0
+	
+	if impatience > 0.2:
+		impatience = 0.0
+		turn_around()
+
 	change_dir_counter -= delta
 	if change_dir_counter < 0:
 		change_direction()
 		reset_change_dir()
+		
+	previous_delta = delta
+	previous_pos = position
 	
 func change_direction():
-	direction = randf_range(-0.2, 0.2)
+	direction = randf_range(-0.6, 0.6)
+	$AnimatedSprite2D.flip_h = false
 
 func reset_change_dir():
 	change_dir_counter = randf_range(change_dir_min_s, change_dir_max_s)
+
+func turn_around():
+	if cos(direction) > 0:
+		direction = direction + PI
+	$AnimatedSprite2D.flip_h = true
+	change_dir_counter = randf_range(change_dir_min_s, change_dir_max_s)
+
